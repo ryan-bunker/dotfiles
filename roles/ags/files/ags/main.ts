c3e50570db7d94eaa00cc1a12213a3b550774c15
Ryan Bunker <ryan.bunker@gmail.com>
2024-06-23 00:18:44 -0500

    Right status area for ags bar

--- roles/ags/files/ags/main.ts
+++ roles/ags/files/ags/main.ts
@@ -1,151 +1,117 @@
-import { Variable as Var } from "types/variable";
-import { Client } from "types/service/hyprland";
+import Workspaces from "workspaces";
 
-const hyprland = await Service.import("hyprland");
 const notifications = await Service.import("notifications");
 const mpris = await Service.import("mpris");
 const audio = await Service.import("audio");
 const battery = await Service.import("battery");
 const systemtray = await Service.import("systemtray");
+const network = await Service.import("network");
 
 const date = Variable("", {
-  poll: [1000, 'date "+%H:%M:%S %b %e."'],
+  poll: [1000, "date '+%a, %b %d'"],
+});
+const time = Variable("", {
+  poll: [1000, "date '+%l:%M %p'"],
 });
 
-const appIcons = {
-  "org.qutebrowser.qutebrowser": "",
-  kitty: "",
-  discord: "",
-};
-
-// widgets can be only assigned as a child in one container
-// so to make a reuseable widget, make it a function
-// then you can simply instantiate one by calling it
-
-const WorkspaceIds = [1, 2, 3, 4, 5, 6, 7, 9];
-
-function Workspaces() {
-  const wsClientVars = WorkspaceIds.reduce(
-    (o: { [id: number]: Var<Client[]> }, id: number) => {
-      const clients = hyprland.clients.filter((c) => c.workspace.id == id);
-      return { ...o, [id]: Variable(clients) };
-    },
-    {},
-  );
-
-  const clientStr = (client?: Client) =>
-    `${client?.address} class=${client?.class} title=${client?.title}`;
-
-  hyprland.connect("client-added", (self, address: string) => {
-    print("client added:", address);
-    const client = self.getClient(address);
-    print("client:", clientStr(client));
-    if (!client) {
-      return;
-    }
-    wsClientVars[client?.workspace.id].value = [
-      ...wsClientVars[client?.workspace.id].value,
-      client,
-    ];
-  });
-  hyprland.connect("client-removed", (_, address: string) => {
-    print("client removed:", address);
-    for (const id of Object.keys(wsClientVars)) {
-      const idx = wsClientVars[id].value.findIndex(
-        (c: Client) => c.address == address,
-      );
-      if (idx < 0) continue;
-      const copy: Client[] = [...wsClientVars[id].value];
-      copy.splice(idx, 1);
-      wsClientVars[id].value = copy;
-      break;
-    }
-    // wsClientVars[client.workspace.id].value = wsClientVars[
-    //   client.workspace.id
-    // ].value.filter((c) => c.address != address);
-  });
-  hyprland.connect("event", (self, name: string, data: string) => {
-    if (name != "movewindowv2") return;
-    const [address, wsId, wsName] = data.split(",");
-    print("window moved:", address, wsId, wsName);
-    const client = self.getClient("0x" + address);
-    print(
-      "client:",
-      clientStr(client),
-      `from=${client?.workspace.id}`,
-      `to=${wsId}`,
-    );
-    if (!client) {
-      return;
-    }
-    const fromId = client.workspace.id;
-    wsClientVars[fromId].value = wsClientVars[fromId].value.filter(
-      (c) => c.address != client?.address,
-    );
-    wsClientVars[wsId].value = [...wsClientVars[wsId].value, client];
-  });
-
-  // const clients = hyprland.clients.filter((c) => c.workspace.id > 0);
-  // const wsLabels = {};
-  // for (const c of clients) {
-  //   wsLabels[c.workspace.id] =
-  //     (wsLabels[c.workspace.id] ?? "") + (appIcons[c.class] ?? `[${c.title}]`);
-  // }
-
-  const activeId = hyprland.active.workspace.bind("id");
-  // const workspaces = hyprland.bind("workspaces").as((ws) =>
-  //   ws.map(({ id }) =>
-  //     Widget.Button({
-  //       on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
-  //       child: Widget.Label(`${id} 󰖟`),
-  //       class_name: activeId.as((i) => `${i === id ? "focused" : ""}`),
-  //     }),
-  //   ),
-  // );
-
-  const workspaces = WorkspaceIds.map((id: number) =>
-    Widget.Button({
-      child: Widget.Box({
-        children: [
-          Widget.Label({
-            label: `${id}`,
-            class_name: "label",
-          }),
-          Widget.Label({
-            label: wsClientVars[id]
-              .bind()
-              .as((clients) =>
-                clients
-                  .map((c) => appIcons[c.class] ?? `[${c.title}]`)
-                  .join(""),
-              ),
-            class_name: "value",
-          }),
-        ],
-      }),
-      class_name: activeId.as((i) =>
-        ["workspace", `ws${id}`, `${i === id ? "focused" : ""}`].join(" "),
+const SystemTray = () =>
+  Widget.Box({
+    spacing: 8,
+    children: systemtray.bind("items").as((items) =>
+      items.map((item) =>
+        Widget.Button({
+          child: Widget.Icon({ size: 20 }).bind("icon", item, "icon"),
+          tooltipMarkup: item.bind("tooltip_markup"),
+          // onPrimaryClick: item.is_menu
+          //   ? undefined
+          //   : (_, event) => item.activate(event),
+          onSecondaryClick: (_, event) => item.openMenu(event),
+        }),
       ),
+    ),
+  });
+
+const VolumeInfo = () =>
+  Widget.EventBox({
+    child: Widget.Icon({ size: 20 }).hook(audio.speaker, (self) => {
+      const vol = audio.speaker.volume * 100;
+      const icon = [
+        { t: 101, i: "oversimplified" },
+        { t: 67, i: "high" },
+        { t: 32, i: "medium" },
+        { t: 1, i: "low" },
+        { t: 0, i: "muted" },
+      ].find(({ t }) => t <= vol)?.i;
+
+      if (audio.speaker.is_muted) {
+        self.icon = "audio-volume-muted-symbolic";
+      } else {
+        self.icon = `audio-volume-${icon}-symbolic`;
+      }
     }),
+  });
+
+const BatteryInfo = () =>
+  Widget.Icon({
+    icon: battery.bind("icon_name"),
+    size: 20,
+    visible: battery.bind("available"),
+  }).hook(battery, (self) => {
+    self.tooltip_text = `${battery.percent}%`;
+    if (battery.charging) {
+      self.tooltip_text += " (charging)";
+    } else if (battery.charged) {
+      self.tooltip_text += " (charged)";
+    }
+  });
+
+const WifiIndicator = () =>
+  Widget.Icon({
+    icon: network.wifi.bind("icon_name"),
+    size: 20,
+  }).hook(
+    network.wifi,
+    (self) => {
+      self.tooltip_text = `${network.wifi.ssid} (${network.wifi.strength} dB)`;
+    },
+    "changed",
   );
 
-  return Widget.Box({
-    class_name: "workspaces",
-    children: workspaces,
+const WiredIndicator = () =>
+  Widget.Icon({
+    icon: network.wired.bind("icon_name"),
   });
-}
 
-function ClientTitle() {
-  return Widget.Label({
-    class_name: "client-title",
-    label: hyprland.active.client.bind("title"),
+const NetworkIndicator = () =>
+  Widget.Stack({
+    children: {
+      wifi: WifiIndicator(),
+      wired: WiredIndicator(),
+    },
+    shown: network.bind("primary").as((p) => p || "wifi"),
   });
-}
 
 function Clock() {
-  return Widget.Label({
-    class_name: "clock",
-    label: date.bind(),
+  return Widget.Box({
+    vertical: true,
+    children: [
+      Widget.Label({ class_name: "clock date", label: date.bind() }),
+      Widget.Label({ class_name: "clock time", label: time.bind() }),
+    ],
+  });
+}
+
+function StatusArea() {
+  return Widget.Box({
+    class_name: "grouping",
+    spacing: 8,
+    children: [
+      SystemTray(),
+      VolumeInfo(),
+      BatteryInfo(),
+      NetworkIndicator(),
+      Clock(),
+    ],
   });
 }
 
@@ -154,7 +120,7 @@ function Clock() {
 function Left() {
   return Widget.Box({
     spacing: 8,
-    children: [Workspaces(), ClientTitle()],
+    children: [Workspaces()],
   });
 }
 
@@ -162,7 +128,7 @@ function Right() {
   return Widget.Box({
     hpack: "end",
     spacing: 8,
-    children: [Clock()],
+    children: [StatusArea()],
   });
 }
 
