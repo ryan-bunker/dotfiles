227960926db18ef531e251a2e5a7a848c7d044be
Ryan Bunker <ryan.bunker@sap.com>
2025-07-25 11:24:06 -0500

    update plugins and add overseer

---
+++ neovim/lua/plugins/overseer.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"stevearc/overseer.nvim",
+		opts = {},
+		-- local parser = require("overseer.parser")
+		-- return parser.new({
+		-- 	{
+		-- 		"loop",
+		-- 		{
+		-- 			"sequence",
+		-- 			{ "extract", { append = false }, "^([^%s].+):(%d+):(%d+): (.*)$", "filename", "lnum", "cnum", "text" },
+		-- 			{ "extract", "^%s*(.*)$", "code" },
+		-- 		},
+		-- 	},
+		-- })
+	},
+}
