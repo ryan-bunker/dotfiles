6f6e4050a75598076ff95bc12a4d91f873903b41
Ryan Bunker <ryan.bunker@gmail.com>
2025-12-20 23:37:12 -0600

    make download script default to using Favorites collection

--- wallpapers/download.sh
+++ wallpapers/download.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+collection_id="${1:-946843}"
 page=0
 
 while :; do
   let "page++"
-  data=$(curl -s "https://wallhaven.cc/api/v1/collections/TheMightyApophis/$1?apikey=$WALLHAVEN_API_KEY&page=$page")
+  data=$(curl -s "https://wallhaven.cc/api/v1/collections/TheMightyApophis/${collection_id}?apikey=${WALLHAVEN_API_KEY}&page=${page}")
   last_page=$(echo $data | jq .meta.last_page)
 
   for url in $(echo $data | jq -r '.data[] | .path')
