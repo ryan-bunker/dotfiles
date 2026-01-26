01cee0448874bc9b2c357423f88b4f0338708163
Ryan Bunker <ryan.bunker@gmail.com>
2026-01-26 01:18:52 -0600

    configure qutebrowser

--- nix/modules/home-manager/qutebrowser.nix
+++ nix/modules/home-manager/qutebrowser.nix
@@ -17,6 +17,21 @@ in {
       searchEngines = {
         DEFAULT = "https://www.google.com/search?hl=en&q={}";
       };
+
+      settings = {
+        auto_save.session = true;
+        colors.webpage.preferred_color_scheme = "dark";
+        fonts = {
+          default_size = "12pt";
+          default_family = "JetBrainsMono Nerd Font Mono";
+          tabs.selected = "12pt Montserrat";
+          tabs.unselected = "12pt Montserrat";
+        };
+      };
+
+      extraConfig = ''
+        c.tabs.padding = {'top': 3, 'bottom': 5, 'right': 5, 'left': 5}
+      '';
     };
   };
 }
