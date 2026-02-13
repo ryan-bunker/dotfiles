18d8e02be6ee04a99d1ad63f63174d33d08cd0d0
Ryan Bunker <ryan.bunker@sap.com>
2026-02-13 10:25:40 -0600

    reduce neovim animations to improve performance

--- nix/modules/home-manager/neovim/lua/config/mini.lua
+++ nix/modules/home-manager/neovim/lua/config/mini.lua
@@ -10,9 +10,18 @@ require("mini.ai").setup({ n_lines = 500 })
 vim.keymap.set("n", "<leader>x", require("mini.bufremove").delete, { desc = "Close the current buffer" })
 
 -- Animate common Neovim actions
-require("mini.animate").setup({
+local animate = require("mini.animate")
+animate.setup({
+	resize = { enable = false },
 	open = { enable = false },
 	close = { enable = false },
+	cursor = {
+		timing = animate.gen_timing.quartic({ duration = 100, unit = "total", control = "out" }),
+	},
+	scroll = {
+		timing = animate.gen_timing.quartic({ duration = 150, unit = "total", control = "out" }),
+		subscroll = animate.gen_subscroll.equal({ max_step = 10 }),
+	},
 })
 
 require("mini.icons").setup()
